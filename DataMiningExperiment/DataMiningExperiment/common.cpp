59c3427085f298b2f9958ef3744e6d403b5e9560
yaoancheng <yaoancheng@ea3ec36c-c97b-11dd-a412-a1792178251d>
2008-12-14 05:53:44 +0000

    优化了数据库投影，给数据库添加了头表。

---
+++ DataMiningExperiment/DataMiningExperiment/common.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "common.h"
+
+#include <set>
+#include <algorithm>
+using namespace std;
+
+bool set_contain(const ItemSet& subSet,const ItemSet& superSet)
+{
+	ItemSet temp;
+		::set_difference(subSet.begin(),
+			subSet.end(),
+			superSet.begin(),
+			superSet.end(),
+			std::insert_iterator<ItemSet>(temp, temp.begin() ));
+
+	return temp.empty();
+}
