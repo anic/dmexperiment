13795331c1fced35e16bc9badbc3e535415fbec1
yaoancheng <yaoancheng@ea3ec36c-c97b-11dd-a412-a1792178251d>
2008-12-17 14:20:56 +0000

    FPtree 做了一些东西

--- DataMiningExperiment/DataMiningExperiment/FPTreeEx.cpp
+++ DataMiningExperiment/DataMiningExperiment/FPTreeEx.cpp
@@ -1,6 +1,7 @@
 #include "StdAfx.h"
 #include "FPTreeEx.h"
 #include <algorithm>
+#include <iostream>
 
 FPTreeEx::FPTreeEx(void)
 {
@@ -24,8 +25,8 @@ void FPTreeEx::createConditionalFPTree(const FPTreeEx& parent,::Item prefix,int 
 	if(parent.header.size() == 0) 
 		return;
 
-	set<fptree::Item>::const_iterator it = parent.header.find(fptree::Item(prefix,0));
-	if (it == header.end())
+	set<fptree::Item>::const_iterator it = parent.header.find(fptree::Item(this->getRemapIndex(prefix),0));
+	if (it == parent.header.end())
 		return;
 
 	//构造一个新的FPTree
@@ -38,6 +39,8 @@ void FPTreeEx::createConditionalFPTree(const FPTreeEx& parent,::Item prefix,int 
 	int *tmp = new int[parent.header.size()];
 	for(i = it->getNext(); i; i = i->nodelink) {
 		int l=0;
+
+		//有问题，不一定是最低的
 		for(fptree::Item_ *p=i->parent; p; p = p->parent) tmp[l++] = p->id; //遍历父亲
 
 		//获得事务的个数
@@ -80,7 +83,7 @@ void FPTreeEx::getHeader(std::vector<::Item>& output,bool bSortedSupport) const
 		for(set<fptree::Item>::const_iterator iter = header.begin();
 			iter!=header.end();
 			++iter)
-			output.push_back(iter->getId());
+			output.push_back(this->remap[iter->getId()]);
 		return;
 	}
 	else
@@ -100,7 +103,7 @@ void FPTreeEx::getHeader(std::vector<::Item>& output,bool bSortedSupport) const
 			iter!=list.end();
 			++iter)
 		{
-			output.push_back((*iter)->id);
+			output.push_back(this->remap[(*iter)->id]);
 		}
 		return;
 	}
@@ -169,4 +172,73 @@ void FPTreeEx::createFromTrDB(const TrDB& trdb,int nMinSupport)
 void FPTreeEx::createFromTrDB(const TrDB& trdb)
 {
 	createFromTrDB(trdb,trdb.getMinSupport());
+}
+
+//获得Item的表，需要访问支持度，根据Item自然排序，并能快速定位
+void FPTreeEx::getHeader(std::map<::Item,int>& output) const
+{
+	for(std::set<fptree::Item>::const_iterator iter = header.begin();
+			iter!=header.end();
+			++iter)
+	{
+		output.insert(std::make_pair(this->remap[iter->getId()],iter->getSupport()));
+	}
+	
+}
+
+bool itemSupGreater(const Item_Support& item1,const Item_Support& item2)
+{
+	return item1.support>item2.support;
+}
+
+//获得Item的表，需要访问支持度，并可以选择根据支持度排降序
+void FPTreeEx::getHeader(std::vector<Item_Support>& output,bool bSortedBySupport) const
+{
+	output.reserve(header.size());
+	for(std::set<fptree::Item>::const_iterator iter = header.begin();
+			iter!=header.end();
+			++iter)
+	{
+		output.push_back(Item_Support(this->remap[iter->getId()],iter->getSupport()));
+	}
+
+	if (bSortedBySupport)
+		std::sort(output.begin(),output.end(),itemSupGreater);
+}
+
+void FPTreeEx::removeItem(::Item item)
+{
+	//TODO: 还没实现
+}
+
+void FPTreeEx::printOnConsole() const
+{
+	if (this->root!=NULL && !root->empty())
+		printNode(*(root->begin()),0);
+}
+
+void FPTreeEx::printNode(const fptree::Item& item, int level) const
+{
+	for(int i=0;i<level;++i)
+		std::cout<<"    ";
+
+	std::cout<<"|_"<<this->remap[item.getId()]<<":"<<item.getSupport();
+	std::cout<<std::endl;
+
+	std::set<fptree::Item> * pChildren = item.getChildren();
+	if (pChildren !=NULL)
+	{
+		for(std::set<fptree::Item>::const_iterator iter = pChildren->begin();
+			iter != pChildren->end();
+			++iter)
+			printNode(*iter,level+1);
+	}
+}
+
+int FPTreeEx::getRemapIndex(::Item item) const 
+{
+	for(int i=0;i<this->len_remap;++i)
+		if (remap[i] == item)
+			return i;
+	return -1;
 }
