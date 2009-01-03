6bbeac4a02bc707f12de0e579562d702724cf6f0
yaoancheng <yaoancheng@ea3ec36c-c97b-11dd-a412-a1792178251d>
2009-01-03 07:55:05 +0000

    修改了测试脚本

--- DataMiningExperiment/DataMiningExperiment/resource/SVMTest_single.bat
+++ DataMiningExperiment/DataMiningExperiment/resource/SVMTest_single.bat
@@ -21,7 +21,7 @@ rem type - 1是用harmony；其他用ddpmine
 rem label - 1是输出标签; 否则输出0
 
 @echo 正在执行算法...
-DataMiningExperiment.exe %TRAIN%_%NUM%_%i%.dat %MIN_SUP% 1 %OUTPUT_DIR%rule%i%.txt %TYPE% 0
+DataMiningExperiment.exe %TRAIN%_%NUM%_%i%.dat %MIN_SUP% 1 %OUTPUT_DIR%rule%i%.txt %TYPE% 1
 
 @echo 进行学习...
 svm_multiclass_learn -c 0.01 %OUTPUT_DIR%rule%i%.txt %OUTPUT_DIR%model%i%.txt
