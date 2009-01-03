5d785fffb844c8a694ba6e9f63fe2207d769c8ee
yaoancheng <yaoancheng@ea3ec36c-c97b-11dd-a412-a1792178251d>
2009-01-03 09:36:59 +0000

    修改测试脚本

--- trunk/DataMiningExperiment/DataMiningExperiment/resource/SVMTest_mushroom_light.bat
+++ trunk/DataMiningExperiment/DataMiningExperiment/resource/SVMTest_mushroom_light.bat
@@ -1,14 +1,14 @@
 @echo off
 rem DIR为测试集所在的路径；如果在当前目录，设置为空;
 
-set TRAIN=mushroom.dat
-set TEST=mushroom_test.dat
+set TRAIN=mushroom_train2.dat
+set TEST=mushroom_test2.dat
 set MIN_SUP=50
 set NUM=10
 set TYPE=1
 rem 3表示1变成-1，2变成1
 @echo 正在执行算法...
-DataMiningExperiment.exe %TRAIN% %MIN_SUP% 1 rule.txt %TYPE% 3
+DataMiningExperiment.exe %TRAIN% %MIN_SUP% 1 rule.txt %TYPE% 1
 
 @echo 进行学习...
 svm_learn rule.txt
