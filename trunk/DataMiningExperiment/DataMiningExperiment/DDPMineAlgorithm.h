8e9ebef61b50399a6e93d9d5880ab17360c7122b
qw04ster <qw04ster@ea3ec36c-c97b-11dd-a412-a1792178251d>
2009-01-04 14:33:25 +0000

    haha, do by walter

--- trunk/DataMiningExperiment/DataMiningExperiment/DDPMineAlgorithm.h
+++ trunk/DataMiningExperiment/DataMiningExperiment/DDPMineAlgorithm.h
@@ -11,13 +11,14 @@ class DDPMineAlgorithm :
 {
 private:
 	ItemSet prefix;
+	double maxIG;
+	TrDB trdb_local;
 	void DDPMine(TrDB& trdb, int min_sup);
     void branch_and_bound(const TrDB& trdb, int min_sup, const std::set<Item>& a);
-	double computeIG(const TrDB& trdb, ItemSet_Support& iset);
+	double computeIG(const TrDB& trdb, ItemSet& iset);
 	void update_tree(TrDB& trdb, const ItemSet& iset);
 	bool getElement(const TrDB& trdb, ItemSet &current);
-	double computeIGup(const TrDB& trdb, ItemSet_Support& iset);
-	void computeTa(const TrDB &trdb, ItemSet &iset);
+	double computeIGup(const TrDB& trdb, ItemSet& iset);
 public:
 	DDPMineAlgorithm(void);
 	~DDPMineAlgorithm(void);
